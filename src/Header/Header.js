226772049420a1c318cc85defc0a9dda782910e9
Ramsey Bland <ramsey.bland02@gmail.com>
2018-08-02 09:26:52 -0400

    Header styling

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -8,17 +8,31 @@ class Header extends Component {
 
     render() {
         return (
-            <div className="header">
+            <header className="header">
+                {/* <i className="material-icons">keyboard_arrow_left</i> */}
+                <h2>Bemus Point Golf Course</h2>
+                <section className="hole-info">
+                    <div className="hole-stat">
+                        <div className="hole-stat-value">4</div>
+                        <div className="hole-stat-description">Par</div>
+                    </div>
+                    <div>
+                        <h3>Hole 1</h3>
+                        <p>365 yards</p>
+                    </div>
+                    <div className="hole-stat">
+                        <div className="hole-stat-value">3</div>
+                        <div className="hole-stat-description">S. Index</div>
+                    </div>
+                </section>
                 <svg x="0px" y="0px" viewBox="0 0 500 250" enableBackground="new 0 0 500 250" preserveAspectRatio="none">
                     <path
                         fill="#FFFFFF"
                         d="M250,246.5c-97.85,0-186.344-40.044-250-104.633V250h500V141.867C436.344,206.456,347.85,246.5,250,246.5z"
                     />
                 </svg>
-                <i className="material-icons">keyboard_arrow_left</i>
-                <h2>Hole 1</h2>
-                <i className="material-icons">keyboard_arrow_right</i>
-            </div>
+                {/* <i className="material-icons">keyboard_arrow_right</i> */}
+            </header>
         );
     }
 }
