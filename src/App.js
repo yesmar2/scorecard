5d937271ccf248dde98af3400064a8b40b7b2f80
Ramsey Bland <ramsey.bland02@gmail.com>
2018-09-10 13:29:15 -0400

    Added styled-components to App component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React, { Component } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import ScorecardVisibility from "./containers/ScorecardVisibility";
-import "./App.css";
+import styled from "styled-components";
+import backgroundImage from "./images/golfer-1960998_1920.jpg";
+
+const AppWrapper = styled.div`
+    height: 100%;
+    background: linear-gradient(rgb(38, 222, 129, 0.9), rgb(38, 222, 129, 0.9)), url(${backgroundImage}) no-repeat center 20% / cover;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: rgba(255, 255, 255, 0.9);
+`;
 
 class App extends Component {
     render() {
         return (
-            <div className="app">
+            <AppWrapper>
                 <ScorecardVisibility />
                 <Header />
                 <Main />
-            </div>
+            </AppWrapper>
         );
     }
 }
